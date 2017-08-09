60242750233aaff88047c95bec0f275cefcf8672
nimtronican <NIMTRONICAN@GMAIL.COM>
2017-08-09 15:09:44 +0530

    Add files via upload

--- lib/settings.php
+++ lib/settings.php
@@ -6,7 +6,8 @@ include("./classes/dbObject.php");
 include("./lib/common.php");
 session_start();
 // db instance
-$db = new Mysqlidb('43.255.154.43:3306', 'mscd_super', 'MSCD12#$@digi', 'mscdigital_taskanalysis');
+$db = new Mysqlidb('148.72.232.138', 'mscd_super', 'MSCD12#$@digi', 'mscdigital_taskanalysis');
+$db->port = 3306;
 // enable class autoloading
 //dbObject::autoload("models");
 
