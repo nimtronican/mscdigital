9205a51f73a758e32e8251f678f5b2633b6f7f28
nimtronican <NIMTRONICAN@GMAIL.COM>
2017-08-23 11:40:38 +0530

    Updated to show all errors

--- lib/settings.php
+++ lib/settings.php
@@ -1,14 +1,18 @@
 <?php
-error_reporting(E_ALL); 
+ini_set('xmlrpc_errors',1);
+ini_set('track_errors',1);
+ini_set('error_reporting',1);
+ini_set('display_startup_errors',1);
 ini_set('display_errors', 1);
+error_reporting(E_ALL); 
 include("./classes/MysqliDb.php");
 include("./classes/dbObject.php");
 include("./lib/common.php");
 session_start();
-// db instance
-//$db = new Mysqlidb('sg2plcpnl0234.prod.sin2.secureserver.net', 'mscd_super', 'MSCD12#$@digi', 'mscdigital_taskanalysis');
-//$db->port = 2083;
-
+// db instance 
+$db = new MysqliDb('sg2plcpnl0234.prod.sin2.secureserver.net', 'mscd_super', 'MSCD12#$@digi', 'mscdigital_taskanalysis');
+$db->port = 3306;
+/*
 /////CLEARDB CONNECTION/////
 $url = parse_url(getenv("mysql://b2ad3d49dad29b:d54eb81f@us-cdbr-sl-dfw-01.cleardb.net:3306/ibmx_903feb6feb9b242?reconnect=true"));
 
@@ -21,5 +25,5 @@ $db = substr($url["path"], 1);
 $db = new Mysqlidb($server, $username, $password, $db);
 $db->port = 3306;
 // enable class autoloading
-//dbObject::autoload("models");
+//dbObject::autoload("models");*/
 ?>
