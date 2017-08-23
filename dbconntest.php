e0b983ece56b569d85e32582107961c8f6f60b15
nimtronican <NIMTRONICAN@GMAIL.COM>
2017-08-23 11:41:30 +0530

    mysqli implementation

--- dbconntest.php
+++ dbconntest.php
@@ -11,12 +11,12 @@ $dbname = 'mscdigital_taskanalysis';
 $dbuser = 'mscd_super';
 $dbpass = 'MSCD12#$@digi';
 $dbhost = 'sg2plcpnl0234.prod.sin2.secureserver.net';
-$connect = mysql_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost' ".mysql_errno($connect));
-mysql_select_db($dbname) or die("Could not open the db '$dbname'".mysql_errno($connect));
+$link = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost' ".mysqli_errno($connect));
+mysqli_select_db($link, $dbname) or die("Could not open the db '$dbname' ".mysqli_errno($connect));
 $test_query = "SHOW TABLES FROM $dbname";
-$result = mysql_query($test_query);
+$result = mysqli_query($link, $test_query);
 $tblCnt = 0;
-while($tbl = mysql_fetch_array($result)) {
+while($tbl = mysqli_fetch_array($result)) {
   $tblCnt++;
   #echo $tbl[0]."<br />\n";
 }
