8d797f60f24aedc2d7a8ef2d709c2a8db71c9d80
nimtronican <NIMTRONICAN@GMAIL.COM>
2017-08-23 11:32:57 +0530

    conntest-testcodes

--- dbconntest.php
+++ dbconntest.php
@@ -1,12 +1,18 @@
 <?php
+ini_set('xmlrpc_errors',1);
+ini_set('track_errors',1);
+ini_set('error_reporting',1);
+ini_set('display_startup_errors',1);
+ini_set('display_errors', 1);
+error_reporting(E_ALL); 
 # Fill our vars and run on cli
 # $ php -f db-connect-test.php
 $dbname = 'mscdigital_taskanalysis';
 $dbuser = 'mscd_super';
 $dbpass = 'MSCD12#$@digi';
 $dbhost = 'sg2plcpnl0234.prod.sin2.secureserver.net';
-$connect = mysql_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
-mysql_select_db($dbname) or die("Could not open the db '$dbname'");
+$connect = mysql_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost' ".mysql_errno($connect));
+mysql_select_db($dbname) or die("Could not open the db '$dbname'".mysql_errno($connect));
 $test_query = "SHOW TABLES FROM $dbname";
 $result = mysql_query($test_query);
 $tblCnt = 0;
