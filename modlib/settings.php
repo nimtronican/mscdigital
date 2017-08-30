0d3742fd6b1691b2c3e66c2d21170496e1e440ac
nimtronican <NIMTRONICAN@GMAIL.COM>
2017-08-30 14:46:44 +0530

    modlib update

--- modlib/settings.php
+++ modlib/settings.php
@@ -7,7 +7,7 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL); 
 include("./classes/MysqliDb.php");
 include("./classes/dbObject.php");
-include("./lib/common.php");
+include("./modlib/common.php");
 session_start();
 // db instance 
 $db = new MysqliDb('sg2plcpnl0234.prod.sin2.secureserver.net', 'mscd_super', 'MSCD12#$@digi', 'mscdigital_taskanalysis');
