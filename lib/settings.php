cee7fe129efb16fd4813498c4fa0182ef5690e4f
nimtronican <NIMTRONICAN@GMAIL.COM>
2017-08-30 11:59:43 +0530

    Native SQL Update

--- lib/settings.php
+++ lib/settings.php
@@ -4,30 +4,15 @@ ini_set('track_errors',1);
 ini_set('error_reporting',1);
 ini_set('display_startup_errors',1);
 ini_set('display_errors', 1);
-error_reporting(E_ALL);
-echo "I am here 1";
-include("./classes/MysqliDb.php");
-include("./classes/dbObject.php");
+error_reporting(E_ALL); 
+//include("./classes/MysqliDb.php");
+//include("./classes/dbObject.php");
 include("./lib/common.php");
 session_start();
-echo "I am here 2";
-// db instance 
-$db = new MysqliDb('sg2plcpnl0234.prod.sin2.secureserver.net', 'mscd_super', 'MSCD12#$@digi', 'mscdigital_taskanalysis');
-$db->port = 3306;
-echo "I am here 2";
-print_r($db);
-/*
-/////CLEARDB CONNECTION/////
-$url = parse_url(getenv("mysql://b2ad3d49dad29b:d54eb81f@us-cdbr-sl-dfw-01.cleardb.net:3306/ibmx_903feb6feb9b242?reconnect=true"));
 
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
-
-//$conn = new mysqli($server, $username, $password, $db);
-$db = new Mysqlidb($server, $username, $password, $db);
-$db->port = 3306;
-// enable class autoloading
-//dbObject::autoload("models");*/
+$dbname = 'mscdigital_taskanalysis';
+$dbuser = 'mscd_super';
+$dbpass = 'MSCD12#$@digi';
+$dbhost = 'sg2plcpnl0234.prod.sin2.secureserver.net';
+$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 ?>
