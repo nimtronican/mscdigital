52944d2b685e40fea5951f8e81f10a7d4a65f18b
nimtronican <NIMTRONICAN@GMAIL.COM>
2017-08-30 14:29:03 +0530

    modlib update

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
-echo "I am outside<br>";
-echo "Whats happening";
-include("./lib/settings.php");
+include("./modlib/settings.php");
 global $ERROR_MSG;
 if($_REQUEST['action'] == NULL){
 	if(isset($_SESSION['login'])){
@@ -10,7 +8,6 @@ if($_REQUEST['action'] == NULL){
 		include("./html/login.html");
 	}
 }else if(isset($_REQUEST['action']) && $_REQUEST['action'] == "register"){
-	echo "I am here loop 2 <br>";
 	if(isset($_SESSION['AUTHCODE']) && isset($_POST['authcode']) && $_POST['authcode'] == $_SESSION['AUTHCODE']){
 		//print_r($_POST);
 		$currID = $_POST['empid'];
@@ -51,55 +48,13 @@ if($_REQUEST['action'] == NULL){
 		global $jobselect;
 		global $tlselect;
 		global $managerselect;
-		echo "I am in 3<br>";
 		$_SESSION['AUTHCODE'] = md5(date("H i s"));
-		echo "I am in 4<br>";
-		//Discipline Select box
-		$teamsquery = 'SELECT * from `msc_discipline` md ORDER BY md.`discipline_name` ASC';
-		$teamsres = $conn->query($teamsquery);
-		if ($teamsres->num_rows > 0) {
-			// output data of each row
-			while($row = $teamsres->fetch_assoc()) {
-				$teamselect .= '<option value="'.$row['id'].'">'.$row['discipline_name'].'</option>';
-			}
-		}
-		//Job role Select box
-		$jobsquery = 'SELECT * from `msc_job` mj ORDER BY mj.`job_name` ASC';
-		$jobsres = $conn->query($jobsquery);
-		if ($jobsres->num_rows > 0) {
-			// output data of each row
-			while($row = $jobsres->fetch_assoc()) {
-				$jobselect .= '<option value="'.$row['id'].'">'.$row['job_name'].'</option>';
-			}
-		}
-		//Team leads Select box
-		$tlsquery = 'SELECT * from `msc_employee` me where me.`emp_level`=1 ORDER BY me.`emp_name` ASC';
-		$tlsres = $conn->query($tlsquery);
-		if ($tlsres->num_rows > 0) {
-			// output data of each row
-			while($row = $tlsres->fetch_assoc()) {
-				$tlselect .= '<option value="'.$row['id'].'">'.$row['emp_name'].'</option>';
-			}
-		}
-		//Managers Select box
-		$managersquery = 'SELECT * from `msc_employee` me where me.`emp_level`=2 ORDER BY me.`emp_name` ASC';
-		$managersres = $conn->query($managersquery);
-		if ($managersres->num_rows > 0) {
-			// output data of each row
-			while($row = $managersres->fetch_assoc()) {
-				$managerselect .= '<option value="'.$row['id'].'">'.$row['emp_name'].'</option>';
-			}
-		}
-		//print_r($teamsres);
-		echo "I am in 6<br>";
-		/*$jobs = dbObject::table("msc_job")->orderBy("job_name","asc")->get();
-		echo "I am in 7<br>";
+		$teams = dbObject::table("msc_discipline")->orderBy("discipline_name","asc")->get();
+		$jobs = dbObject::table("msc_job")->orderBy("job_name","asc")->get();
 		$employee = dbObject::table("msc_employee");
-		echo "I am in 8<br>";
 		$teamleads = $employee::where("emp_level","1")->orderBy("emp_name","asc")->get();
-		echo "I am in 9<br>";
 		$managers = $employee::where("emp_level","2")->orderBy("emp_name","asc")->get();
-		/*foreach($teams as $t){
+		foreach($teams as $t){
 			$teamselect .= '<option value="'.$t->id.'">'.$t->discipline_name.'</option>';
 		}
 		foreach($jobs as $j){
@@ -112,10 +67,8 @@ if($_REQUEST['action'] == NULL){
 		}
 		foreach ($managers as $m){
 				$managerselect .= '<option value="'.$m->id.'">'.$m->emp_name.'</option>';
-		}*/
+		}
 	}
-	$conn->close();
-	echo "I am in 9<br>";
 	include("./html/register.html");
 }else{
 	echo "Unauthenticated Access";
