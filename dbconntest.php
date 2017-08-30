4a32c4fc38cad7df4d53649a5575696aba0f0a04
nimtronican <NIMTRONICAN@GMAIL.COM>
2017-08-30 11:38:45 +0530

    updated connection test with native sql docs

--- dbconntest.php
+++ dbconntest.php
@@ -5,23 +5,20 @@ ini_set('error_reporting',1);
 ini_set('display_startup_errors',1);
 ini_set('display_errors', 1);
 error_reporting(E_ALL); 
-# Fill our vars and run on cli
-# $ php -f db-connect-test.php
+
 $dbname = 'mscdigital_taskanalysis';
 $dbuser = 'mscd_super';
 $dbpass = 'MSCD12#$@digi';
 $dbhost = 'sg2plcpnl0234.prod.sin2.secureserver.net';
-$link = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost' ".mysqli_errno($link));
-mysqli_select_db($link, $dbname) or die("Could not open the db '$dbname' ".mysqli_errno($link));
-$test_query = "SHOW TABLES FROM $dbname";
-$result = mysqli_query($link, $test_query);
-$tblCnt = 0;
-while($tbl = mysqli_fetch_array($result)) {
-  $tblCnt++;
-  #echo $tbl[0]."<br />\n";
-}
-if (!$tblCnt) {
-  echo "There are no tables<br />\n";
-} else {
-  echo "There are $tblCnt tables<br />\n";
-}
+$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+//$dblink = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost' ".mysqli_errno($dblink));
+//mysqli_select_db($dblink, $dbname) or die("Could not open the db '$dbname' ".mysqli_errno($dblink));
+$test_query = 'SELECT * from `msc_discipline` md ORDER BY md.`discipline_name` ASC';
+$result = $conn->query($test_query);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["discipline_name"]. "<br>";
+    }
+} 
+?>
