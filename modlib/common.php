b591dec8fbdd3f0ae9bf8ba3ffefdd41346bad56
nimtronican <NIMTRONICAN@GMAIL.COM>
2017-08-31 19:13:33 +0530

    Major update 2

--- modlib/common.php
+++ modlib/common.php
@@ -11,4 +11,26 @@ function getFreqValue($val){
 	
 	return $ret;
 }
+function getEmpLevel($val){
+	if($val == 0)
+		$ret = "Individual Contributor";
+	else if($val = 110)
+		$ret = "Team Leader";
+	else if($val = 111)
+		$ret = "Manager";
+	else
+		$ret = "Unknown";
+	
+	return $ret;
+}
+
+//DB Functions
+function getLoginDetails($db,$uname,$psw){
+	$ret = $db->rawQuery('SELECT me.*,md.`discipline_name`,mj.`job_name` FROM `msc_employee` me,`msc_discipline` md,`msc_job` mj WHERE me.emp_ibmid ="'.$psw.'" AND me.emp_email = "'.$uname.'" AND md.id =me.emp_discipline AND mj.id = me.emp_job');
+	return $ret[0];
+}
+function getDiscJob($db,$discid,$jobid){
+	$ret = $db->rawQuery('SELECT md.`discipline_name`,mj.`job_name` FROM `msc_discipline` md,`msc_job` mj WHERE md.id ="'.$discid.'" AND mj.id = "'.$jobid.'"');
+	return $ret[0];
+}
 ?>
--- modlib/settings.php
+++ modlib/settings.php
@@ -1,26 +1,25 @@
 <?php
-ini_set('xmlrpc_errors',1);
+/*ini_set('xmlrpc_errors',1);
 ini_set('track_errors',1);
 ini_set('error_reporting',1);
 ini_set('display_startup_errors',1);
 ini_set('display_errors', 1);
-error_reporting(E_ALL); 
+error_reporting(E_ALL); */
 include("./classes/MysqliDb.php");
 include("./classes/dbObject.php");
 include("./modlib/common.php");
 session_start();
+//print_r($_SESSION);
 // db instance 
 $db = new MysqliDb('sg2plcpnl0234.prod.sin2.secureserver.net', 'mscd_super', 'MSCD12#$@digi', 'mscdigital_taskanalysis');
 $db->port = 3306;
 /*
 /////CLEARDB CONNECTION/////
 $url = parse_url(getenv("mysql://b2ad3d49dad29b:d54eb81f@us-cdbr-sl-dfw-01.cleardb.net:3306/ibmx_903feb6feb9b242?reconnect=true"));
-
 $server = $url["host"];
 $username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"], 1);
-
 //$conn = new mysqli($server, $username, $password, $db);
 $db = new Mysqlidb($server, $username, $password, $db);
 $db->port = 3306;
